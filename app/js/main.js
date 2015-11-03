2f1d96f5a5a596980c39e515b4e226e82ed1360b
Lauren Kelly <hi@laurenkelly.me>
2015-11-03 19:04:37 +0100

    Book foldout slidesr x2 done

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
      slidesToShow: 1,
      slidesToScroll: 1,
      autoplay: true,
-     autoplaySpeed: 3000,
+     autoplaySpeed: 2000,
+     speed: 0,
+     dots: true
    });
 });
