7ff74991a085158350c7251f5ef6ff42fff79066
Lauren Kelly <hi@laurenkelly.me>
2015-11-03 14:38:56 +0100

    inlude slick js plugin for book image fold carosel

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
 	$('.work-list li .element').hoverdir();
 });
+
+$(document).ready(function(){
+   $('.project-imagesfold').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     autoplay: true,
+     autoplaySpeed: 3000,
+   });
+});
