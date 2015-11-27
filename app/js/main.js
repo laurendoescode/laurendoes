abdd56f08c5acdd782cfb6b0ecea69bc6cccbfa1
Charles Bancroft <contact@charlesbancroft.info>
2015-11-27 21:40:59 +0100

    Bugfixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,42 @@
-$(function () {
-	$('.work-list li .element').hoverdir();
-});
-
 $(document).ready(function(){
-   $('.project-imagesfold').slick({
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     autoplay: true,
-     autoplaySpeed: 2000,
-     speed: 0,
-     dots: true
-   });
-});
+    $('.project-imagesfold').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 0,
+        dots: true
+    });
 
-var svg1 = $('svg#heading1-underline').drawsvg({reverse: true});
+    $('.work-list li .element').hoverdir();
 
-$('.home-heading1').on('mouseover', function() {
-	$('svg#heading1-underline').parent('.svg-wrapper').show();
-	$('svg#heading1-underline').drawsvg('animate');
-});
+    $('.home-heading1, .home-heading2, .home-heading3').on('mouseover', function(ev) {
+        $(ev.target).siblings('svg').css('visibility', 'visible');
+        $(ev.target).siblings('svg').drawsvg({reverse: true}).drawsvg('animate');
+    });
 
-$('.home-heading1').on('mouseout', function() {
-	$('svg#heading1-underline').parent('.svg-wrapper').hide();
-});
+    $('.home-heading4').on('mouseover', function(ev) {
+        $(ev.target).siblings('svg').css('visibility', 'visible');
+        $(ev.target).siblings('svg').drawsvg({reverse: true}).drawsvg('animate');
+    });
 
-$('.collapse-trigger').on('click', function() {
-	$('.collapse-trigger').siblings(".collapse").slideDown(400);
-});
+    $('.home-heading1, .home-heading2, .home-heading3, .home-heading4').on('mouseout', function(ev) {
+        $(ev.target).siblings('svg').css('visibility', 'hidden');
+    });
 
-var svg2 = $('svg#heading2-underline').drawsvg({reverse: false});
+    $('.collapse-trigger').on('click', function() {
+        $('.collapse-trigger').hide();
+        $('.collapse-trigger').siblings(".collapse").slideDown(400);
+    });
 
-$('.home-heading2').on('mouseover', function() {
-    $('svg#heading2-underline').parent('.svg-wrapper').show();
-    svg2.drawsvg('animate');
-});
-
-$('.home-heading2').on('mouseout', function() {
-    $('svg#heading2-underline').parent('.svg-wrapper').hide();
+    $('.site-subtitle').on('click', function(ev) {
+        var target = $('.home-work');
+        if (target) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+        ev.preventDefault();
+    });
 });
